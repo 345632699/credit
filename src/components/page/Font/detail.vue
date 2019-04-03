de2facd23eea0f47dc56071e7029494b14743560
xuguangfu <345632369@qq.com>
2019-04-04 01:31:56 +0800

    ddd

--- src/components/page/Font/detail.vue
+++ src/components/page/Font/detail.vue
@@ -9,19 +9,19 @@
                              alt="">
                     </el-col>
                     <el-col :span="6">
-                        <el-input
-                                class="search"
-                                popper-class="my-autocomplete"
-                                v-model="pageInit.keyword"
-                                placeholder="请输入内容"
-                        >
-                            <i
-                                    class="el-icon-search el-input__icon"
-                                    slot="suffix"
-                                    style="cursor: pointer;"
-                                    @click="handleIconClick">
-                            </i>
-                        </el-input>
+                        <!--<el-input-->
+                                <!--class="search"-->
+                                <!--popper-class="my-autocomplete"-->
+                                <!--v-model="pageInit.keyword"-->
+                                <!--placeholder="请输入内容"-->
+                        <!--&gt;-->
+                            <!--<i-->
+                                    <!--class="el-icon-search el-input__icon"-->
+                                    <!--slot="suffix"-->
+                                    <!--style="cursor: pointer;"-->
+                                    <!--@click="handleIconClick">-->
+                            <!--</i>-->
+                        <!--</el-input>-->
                     </el-col>
                 </el-row>
             </div>
@@ -248,7 +248,7 @@
                         <el-input
                                 class="inner-search"
                                 popper-class="my-autocomplete"
-                                v-model="state3"
+                                v-model="pageInit.keyword"
                                 placeholder="请输入内容"
                                 style="width: 300px"
                         >
@@ -256,7 +256,7 @@
                                     class="el-icon-search el-input__icon"
                                     slot="suffix"
                                     style="cursor: pointer;"
-                                    @click="searchIn">
+                                    @click="handleIconClick">
                             </i>
                         </el-input>
                     </div>
@@ -785,6 +785,7 @@ export default {
                     }
 
                     .descripiton {
+                        padding: 20px;
                         /*margin-top: 20px;*/
                     }
                 }
